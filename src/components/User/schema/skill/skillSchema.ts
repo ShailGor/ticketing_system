c4c5bc6b1018b17933e9bdf109d98de7093a24a3
Shail Gor <shail.gor@smartsensesolutions.com>
2022-04-28 18:06:53 +0530

    Modified some files

---
+++ src/components/User/schema/skill/skillSchema.ts
@@ -0,0 +1,40 @@
+import 'dotenv/config';
+import { DataTypes, Model } from 'sequelize';
+import sequelize from '../../../../utils/dbConfig';
+
+export class Skill extends Model {
+    public id!: number;
+    public skill!: string;
+}
+
+Skill.init(
+    {
+        id: {
+            type: DataTypes.BIGINT.UNSIGNED,
+            autoIncrement: true,
+            primaryKey: true,
+        },
+        skill: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+    {
+        sequelize,
+        modelName: 'Skill',
+        tableName: 'skills',
+        paranoid: true,
+        createdAt: 'created_at',
+        updatedAt: 'updated_at',
+        deletedAt: 'deleted_at',
+    }
+);
+
+// let data = async () => {
+//     let data = Skill.create({
+//         skill: 'javascript',
+//     });
+//     return data;
+// };
+
+// console.log(data());
